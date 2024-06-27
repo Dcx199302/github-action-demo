22cb8a9a94f5f0e9d1964c765f7f3a5039ff5ef0
Dcx199302 <61185183+Dcx199302@users.noreply.github.com>
2024-06-27 12:50:48 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue Logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      Hello Vue3!!
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
