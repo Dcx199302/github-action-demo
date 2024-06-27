56faa1057b49b9ff194debb025bcda5892d5d5ea
Dcx199302 <61185183+Dcx199302@users.noreply.github.com>
2024-06-27 13:23:14 +0800

    save

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/github-action-demo/",
+  // base: "/github-action-demo/",
   plugins: [vue()],
   resolve: {
     alias: {
