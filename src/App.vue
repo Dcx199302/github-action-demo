3477952f2c2089f16af011bcdda03b0ae3fd3220
Dcx199302 <61185183+Dcx199302@users.noreply.github.com>
2024-06-27 12:54:48 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      Hello Vue3!!
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
